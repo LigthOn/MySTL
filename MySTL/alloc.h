bef276bd746d4c05479950d7628b70074f1cf91f
Marx <15298393123@163.com>
2022-04-09 22:24:24 +0800

    update alloc.h

--- MySTL/alloc.h
+++ MySTL/alloc.h
@@ -20,34 +20,47 @@ namespace mystl    //命名空间: 建立一些互相分隔的作用域，把一
 {
     //free-lists 
     /*为了避免额外的开销（额外的指针指向下一个节点），在存储free-lists时选用union进行存储。*/
-
     union freeList
     {
         union freeList* next;  //指向下一个空闲区块
         char data[1];  //存储当前空间区块的首地址
     };
 
-
     //lists size 的限制参数
     //疑问：为什么不用int， 而是使用 enum？
     enum {_ALIGN = 8};   //区块的上调边界
     enum {_MAX_BYTES = 128};    //区块大小的最大值
-    enum {_FREELISTS_NUM = _MAX_BYTES / _ALIGN};
+    enum {_FREELISTS_NUM = _MAX_BYTES / _ALIGN};  //区块size的个数
 
 
     class _alloc_mystl
     {
         private:
-        //ROUND UP 上调区块需求，满足8的倍数
+        //征对不同size的区块，定义16个freeLists
+        //添加了 volatile 参数，表示该列表通常改变。
+        static freeList* volatile free_lists[_FREELISTS_NUM];
+
+        //ROUND UP 上调区块需求，满足8的倍数，使用静态可以减少实例化时额外的开销
         static size_t ROUND_UP(size_t bytes)
         {
-            
+            return (((bytes) + (_ALIGN - 1)) &  ~(_ALIGN - 1));   //虽然没有搞懂原理，但是验证了一下（位运算） 
         }
-    }
 
+        //根据 区块的需求大小，获取其在 free_lists 中的index
+        static size_t FREELISTS_INDEX(size_t bytes)
+        {
+            return (((bytes) + (_ALIGN - 1)) / (_ALIGN) - 1);
+        }
+
+        //重新填充 free_lists  !!!待更新
+        static void* refill();
+
+
+        //基本的 allocate 以及 deallocate 函数
+        public:
+        static void* allocate(size_t n);
+        static void* deallocate(void* p, size_t n);
+    };
 
 }
-
-
-
 #endif
