acda5c3568e0f7e9be17f55aa3718dc191a9b90f
Marx <15298393123@163.com>
2022-04-09 20:53:31 +0800

    update alloc.h

--- MySTL/alloc.h
+++ MySTL/alloc.h
@@ -19,7 +19,33 @@
 namespace mystl    //命名空间: 建立一些互相分隔的作用域，把一些全局实体分隔开来。
 {
     //free-lists 
-    
+    /*为了避免额外的开销（额外的指针指向下一个节点），在存储free-lists时选用union进行存储。*/
+
+    union freeList
+    {
+        union freeList* next;  //指向下一个空闲区块
+        char data[1];  //存储当前空间区块的首地址
+    };
+
+
+    //lists size 的限制参数
+    //疑问：为什么不用int， 而是使用 enum？
+    enum {_ALIGN = 8};   //区块的上调边界
+    enum {_MAX_BYTES = 128};    //区块大小的最大值
+    enum {_FREELISTS_NUM = _MAX_BYTES / _ALIGN};
+
+
+    class _alloc_mystl
+    {
+        private:
+        //ROUND UP 上调区块需求，满足8的倍数
+        static size_t ROUND_UP(size_t bytes)
+        {
+            
+        }
+    }
+
+
 }
 
 
