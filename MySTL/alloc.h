7b40cec106442ba7d1943c8865d8043a8a5efef9
Marx <15298393123@163.com>
2022-04-11 17:05:20 +0800

    update alloc.h

--- MySTL/alloc.h
+++ MySTL/alloc.h
@@ -56,12 +56,27 @@ namespace mystl    //命名空间: 建立一些互相分隔的作用域，把一
         static void* refill(size_t n);
         static char* chunk_alloc(size_t n, int& nfreelist_links);
 
+        //当前内存池空闲空间的起始地址
+        static char* start_free_pool;
+        static char* end_free_pool;
+        static size_t heap_size; //用于记录向系统heap申请内存空间大小总和
+
 
         //基本的 allocate 以及 deallocate 函数
         public:
         static void* allocate(size_t n);
         static void deallocate(void* p, size_t n);
+        static void* reallocte(size_t old_n, void* p, size_t new_n);
     };
+
+    //static 变量初始化
+    freeList* volatile _alloc_mystl::free_lists[_FREELISTS_NUM] = 
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    char* _alloc_mystl::start_free_pool = 0;
+    char* _alloc_mystl::end_free_pool = 0;
+    size_t _alloc_mystl::heap_size = 0;
+    
 
 
     //分配需求为 n
@@ -92,7 +107,6 @@ namespace mystl    //命名空间: 建立一些互相分隔的作用域，把一
         }
     }
 
-
     void _alloc_mystl::deallocate(void *p, size_t n)
     {
         freeList* volatile *my_free_list;
@@ -113,6 +127,12 @@ namespace mystl    //命名空间: 建立一些互相分隔的作用域，把一
         }
     }
 
+    void* _alloc_mystl::reallocte(size_t old_n, void* p, size_t new_n)
+    {
+        deallocate(p, old_n);
+        p = allocate(new_n);
+        return p;
+    }
     /*
     在调用allocate时，若没有空闲空间，则掉用refill来进行free-lists的更新，并返回一个大小为n的区块
     新的区块空间取自内存池，经由 chunk_alloc() 完成。
@@ -128,22 +148,116 @@ namespace mystl    //命名空间: 建立一些互相分隔的作用域，把一
         char* chunk = chunk_alloc(n, nfreelist_links);
 
         freeList* volatile *my_free_list;
+        freeList* result;    //返回分配的空闲区块
+        freeList* curr_link, * next_link;
 
         if(nfreelist_links == 1)    //只能申请到一个符合条件的区块,即chunk
         {
             return chunk;
         }
-        else      //返回多个区块，将其放入到 free-lists 中
-        {
-            my_free_list = free_lists + FREELISTS_INDEX(n);
+        else   //获取的chunk可能是一大片区块，可后续分为返回多个区块，将其放入到 free-lists 中
+        {   
+            result = reinterpret_cast<freeList*>(chunk);  //返回给请求对象
+
+            //将分配都的chunk 更新到对应的 free-list 中
+            my_free_list = free_lists + FREELISTS_INDEX(n);  
+            *my_free_list = next_link = reinterpret_cast<freeList*>(chunk + n);  
+            
+            //前 n bytes (0号区块）分配给result,后续（1 ~ nfreelist_links-1）的更新到free-list中
+            for(int i = 1; ; ++i)
+            {
+                curr_link = next_link;   //此时 curr_link 已经指向了 my_free_list
+                next_link = reinterpret_cast<freeList*>((char *)curr_link + n);  //(char *)curr_link 取地址
+
+                if(i == nfreelist_links-1)  //最后一个区块
+                {
+                    curr_link->next = 0;
+                    break;
+                }
+                else
+                {
+                    curr_link->next = next_link;
+                }
+            }
         }
-
-
-
-        
-
-
+        return result;
     }
 
+    //因为要根据内存池具体的区块供应能力修改nfreelist_links，所有使用 引用的方式传递参数
+    char* _alloc_mystl::chunk_alloc(size_t n, int& nfreelist_links)   //返回的是首地址
+    {
+        char* result;
+        size_t required_bytes = n * nfreelist_links;
+        size_t left_bytes = end_free_pool - start_free_pool;  //当前内存池中剩余的空闲空间大小
+
+        if(left_bytes >= required_bytes)  //足够进行分配
+        {
+            result = start_free_pool;
+            start_free_pool += required_bytes;
+            return result;
+        }
+        else if(left_bytes >= n) //空间不够，但是能够分配一个
+        {
+            nfreelist_links = left_bytes / n;
+            required_bytes = nfreelist_links * n;
+            result = start_free_pool;
+            start_free_pool += required_bytes;
+            return result;
+        }
+        else  //一个满足需求的区块都无法分配
+        {
+            //先将当前剩余的空间 分配到 合适的 free-list中(肯定是只剩一个)
+            if(left_bytes > 0)  //剩余价值利用
+            {
+                freeList* volatile *my_free_list = free_lists + FREELISTS_INDEX(left_bytes);
+                reinterpret_cast<freeList*>(start_free_pool)->next = *my_free_list;
+                *my_free_list = reinterpret_cast<freeList*>(start_free_pool);
+            }
+
+            //配置heap空间（向系统堆申请空间），用来补充内存池
+            //bytes_to_get 需要向heap申请的空间的大小
+            /*一般分配 required_bytes * 2
+              同时为了应对越来越大的需求，根据请求的增多，扩大申请空间的大小(利用ROUND_UP(heap_size >> 4))
+            */
+            /*通过heap更新 start_free_pool 以及 end_free_pool，
+              此时就是先更新了 内存池，
+              然后再掉用 chunk_alloc 函数，在更新后的内存池中找到空间区用于 refill中更新 free-list
+            */
+
+            size_t bytes_to_get = required_bytes * 2 + ROUND_UP(heap_size >> 4);
+            start_free_pool = (char *)std::malloc(bytes_to_get);
+
+            if(start_free_pool == 0) //heap中也没有可用空间了
+            {
+                /*看看free-lists中还有没有剩余的其他的(size > n)的区块,
+                  如果有的话，可以拿过来用一用*/
+                
+                for(int i = n; i < _MAX_BYTES; i += _ALIGN)
+                {
+                    freeList* volatile* my_free_list = free_lists + FREELISTS_INDEX(i);
+                    freeList* p = *my_free_list;
+
+                    if(p != 0) //找到了一个未使用的空间
+                    {
+                        *my_free_list = p->next;  //记得更新所占用的那个 free-list
+                        start_free_pool = (char*)p;
+                        end_free_pool = start_free_pool+i;
+
+                        //递归自己，修正nfreelist_links,保证编入 free-list的没有多余的空间，而是正好是n
+                        return chunk_alloc(n, nfreelist_links);  
+                    }
+                }
+                //遍历free-lists之后还是没有找到空间的区块
+                std::printf("OUT OF MEMORY!");
+                throw std::bad_alloc();   //抛出异常
+            }
+            else
+            {
+                heap_size += bytes_to_get;
+                end_free_pool = start_free_pool + bytes_to_get;
+                return chunk_alloc(n, nfreelist_links);
+            }
+        }
+    }
 }
 #endif
