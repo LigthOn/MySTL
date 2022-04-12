94940163e9810eda97b87ab29500d568002d4a21
Marx <15298393123@163.com>
2022-04-12 23:11:58 +0800

    update construct.h

--- MySTL/construct.h
+++ MySTL/construct.h
@@ -5,7 +5,18 @@
 
 namespace mystl 
 {
-    
+    template <typename T1, typename T2>
+    inline void* construct(T1* p, T2& value)
+    {
+        new(p) T1(value);     //掉用 T1::T1(value)
+    }
+
+
+    template <typename T>
+    inline void destroy(T* pointer)
+    {
+        pointer->~T();
+    }
 }
 
 #endif
